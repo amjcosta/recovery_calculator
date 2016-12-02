f26d40c5d3abdded82e60bc1c6d86f6a8291348b
Amy Costa <costa.amyj@gmail.com>
2016-12-01 21:53:11 -0500

    Added tests.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,6 +1,8 @@
 require 'rails_helper'
+require 'spec_helper'
 
 RSpec.describe PagesController, type: :controller do
+  render_views
 
   describe "GET #home" do
     it "returns http success" do
@@ -16,4 +18,10 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  describe "GET #about" do
+  	it "returns http success" do
+  	  get :about
+  	  expect(response).to have_http_status(:success)
+  	end
+  end
 end
