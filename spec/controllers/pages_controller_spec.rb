fde2d2a9ba5e80f1bb6e81bbd2f4e5bbfdf1444b
Amy Costa <costa.amyj@gmail.com>
2016-12-04 16:44:43 -0500

    Adding content to pages and refactoring.

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,5 +1,6 @@
 require 'rails_helper'
 require 'spec_helper'
+include Capybara::RSpecMatchers
 
 RSpec.describe PagesController, type: :controller do
   render_views
@@ -11,6 +12,11 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  it "should have the right title" do
+  	get :home
+  	expect(response.body).to have_title("Eating Disorder Recovery Calculator | Home")
+  end
+
   describe "GET #contact" do
     it "returns http success" do
       get :contact
@@ -18,10 +24,20 @@ RSpec.describe PagesController, type: :controller do
     end
   end
 
+  it "should have the right title" do
+  	get :contact
+  	expect(response.body).to have_title("Eating Disorder Recovery Calculator | Contact")
+  end
+
   describe "GET #about" do
   	it "returns http success" do
   	  get :about
   	  expect(response).to have_http_status(:success)
   	end
+  end
+
+  it "should have the right title" do
+  	get :about
+  	expect(response.body).to have_title("Eating Disorder Recovery Calculator | About")
   end
 end
